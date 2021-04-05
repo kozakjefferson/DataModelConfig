05616180ac9ca2b65440fb72542629708b07bece
kozakjefferson <jnkkozak@gmail.com>
2021-04-05 21:38:09 +0100

    files

--- cpfiles.sh
+++ cpfiles.sh
@@ -4,7 +4,10 @@ for dir in $(ls 15_Source_Inventory/*);do
       filepath=$(echo $file | sed 's/.json//g').txt;
       file_target="ods"
       echo "export SANDBOX=/home/sandbox/poc/jkozak/power_one/source/"$dir/$file_target > 15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/$filepath
-      rm  15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/$file
+      rm -rf 15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/$file
+      rm -rf 15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/15*
+      rm -rf 15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/Source*
+      rm -rf 15_Source_Inventory/SOURCE_INTERFACE_02_05_000/$dir/INTERFACE*
     fi;
   done;
 done
